d3e2794ae608ac12bdd34bc883dccd9c984aee9a
YU-HSUAN TSENG <38046774+steven0323@users.noreply.github.com>
2021-07-13 09:06:18 +0800

    Updates

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ class App extends React.Component {
     }
   }
 }
-//ReactDOM.render(<App/>, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(
   <h1>Hello, world from index.js</h1>,
   document.getElementById('root')
