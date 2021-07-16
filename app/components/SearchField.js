db09eeb2e690d6503e7ba6f1de34d9722293e629
steven0323 <38046774+steven0323@users.noreply.github.com>
2021-07-16 17:19:25 +0800

    deploy

--- app/components/SearchField.js
+++ app/components/SearchField.js
@@ -9,7 +9,7 @@ import Search from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
 import AlertDialog from './AlertDialog';
-import HomePageImg from './HomePage3.PNG';
+import HomePageImg from './homepage3.png';
 import AutoSuggestions from './AutoSuggestions';
 
 const styles = ({
